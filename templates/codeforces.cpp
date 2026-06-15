0ef0853c8776197fed1b7971bfadde6e5d913039
Louie Torres <dev@lowkeytowers.com>
2026-06-15 18:43:59 +0800

    feat: add codeforces template

---
+++ templates/codeforces.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void solve() {
+  return;
+}
+
+int main(void) {
+  ll T;
+  cin >> T;
+
+  while (T--) {
+    solve();
+  }
+
+  return 0;
+}
